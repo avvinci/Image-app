89140cd850eaf6e93e5fc8f80ef453000e5a0538
avvinci <vinciabhinav7@gmail.com>
2019-04-20 18:21:39 +0530

    drag working on all images

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 
-  let arr = [] ; 
-
+let arr = [] ; 
 
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
@@ -21,13 +20,15 @@ function handleFileSelect(evt) {
         return function(e) {
           // Render thumbnail.
           var span = document.createElement('span');
-          let id = "myimg" +i ; 
+          let id = "myimg" + count++ ; 
           span.innerHTML = ['<img class="thumb"  draggable="true" ondragstart="drag(event)" src="', e.target.result,
                             '"id="', id,
                             '" title="', escape(theFile.name), '"/>'].join('');
           document.getElementById('list').insertBefore(span, null);
           diplayImage(id) ; 
-          arr.concat(id) ; 
+          // console.log(id) ;
+          moveImg(id) ; 
+          // arr.push(id) ; 
           // console.log(document.getElementById(id)) ; 
         };
       })(f);
@@ -35,6 +36,15 @@ function handleFileSelect(evt) {
       // Read in the image file as a data URL.
       reader.readAsDataURL(f);
     }
+    
+    
+    // for (let i = 0 ; i< arr.length;i++){
+    //   let id = "myimg" + i ; 
+    //   console.log(id) ;
+
+    // }
+    // console.log(arr) ; 
+    
     
   }
 
@@ -47,46 +57,46 @@ function handleFileSelect(evt) {
       // let currentDroppable = null;
   }
 
-  console.log(arr) ; 
-  for (i = 0 ; i< arr.length;i++){
-      moveImg(arr[i]) ; 
-  }
+
+
 
   function moveImg(id){
+    console.log(document.getElementById(id)) ; 
+    
     let ball = document.getElementById(id) ; 
 
-      ball.onmousedown = function(event) {
-    
-        let shiftX = event.clientX - ball.getBoundingClientRect().left;
-        let shiftY = event.clientY - ball.getBoundingClientRect().top;
-    
-        ball.style.position = 'absolute';
-        ball.style.zIndex = 1000;
-        document.body.append(ball);
-    
+    ball.onmousedown = function(event) {
+  
+      let shiftX = event.clientX - ball.getBoundingClientRect().left;
+      let shiftY = event.clientY - ball.getBoundingClientRect().top;
+  
+      ball.style.position = 'absolute';
+      ball.style.zIndex = 1000;
+      document.body.append(ball);
+  
+      moveAt(event.pageX, event.pageY);
+  
+      function moveAt(pageX, pageY) {
+        ball.style.left = pageX - shiftX + 'px';
+        ball.style.top = pageY - shiftY + 'px';
+      }
+  
+      function onMouseMove(event) {
         moveAt(event.pageX, event.pageY);
-    
-        function moveAt(pageX, pageY) {
-          ball.style.left = pageX - shiftX + 'px';
-          ball.style.top = pageY - shiftY + 'px';
-        }
-    
-        function onMouseMove(event) {
-          moveAt(event.pageX, event.pageY);
-        }
-    
-        document.addEventListener('mousemove', onMouseMove);
-    
-        ball.onmouseup = function() {
-          document.removeEventListener('mousemove', onMouseMove);
-          ball.onmouseup = null;
-        };
-    
+      }
+  
+      document.addEventListener('mousemove', onMouseMove);
+  
+      ball.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        ball.onmouseup = null;
       };
+  
+    };
 
-      ball.ondragstart = function() {
-        return false;
-      };
+    ball.ondragstart = function() {
+      return false;
+    };
   }
 
 
